db4456dd26d8b36962ee6bd7331720deaa5de51b
dimitry <dimitry@google.com>
2025-04-07 17:24:01 +0200

    GuestCodeRegion: remove unreferenced block on ResolveEdges
    Bug: 363582841
    Test: run unit tests
    Test: run benchmarks, collect heavy translated region sized check
          for absence of divergences.
    Change-Id: I0a2cdc9ea168111dfa9a31e8f3d021078b8caae9

--- runtime_primitives/guest_code_region.cc
+++ runtime_primitives/guest_code_region.cc
@@ -17,11 +17,14 @@
 #include "berberis/runtime_primitives/guest_code_region.h"
 
 #include <cstddef>
+#include <string>
 
 #include "berberis/base/arena_alloc.h"
+#include "berberis/base/arena_list.h"
 #include "berberis/base/arena_map.h"
 #include "berberis/base/arena_set.h"
 #include "berberis/base/arena_vector.h"
+#include "berberis/base/stringprintf.h"
 #include "berberis/guest_state/guest_addr.h"
 
 namespace berberis {
@@ -40,8 +43,72 @@ void GuestCodeRegion::ResolveEdges() {
   CHECK(!code_region_finalized_);
   ValidateRegionBeforeFinalize();
   SplitBasicBlocks();
+  // SplitBasicBlocks can end up splitting a block
+  // into referenced and unreferenced ones, so it
+  // needs to happen before RemoveUnreferencedBlocks.
+  RemoveUnreachableBlocks();
   ResolveInEdges();
   code_region_finalized_ = true;
+}
+
+ArenaSet<GuestAddr> GuestCodeRegion::CollectReachableBranchTargets() const {
+  ArenaSet<GuestAddr> reachable_targets(arena_);
+  ArenaList<GuestAddr> worklist_targets(arena_);
+
+  const GuestAddr kRegionStartAddr = basic_blocks_.begin()->first;
+  const GuestAddr kRegionEndAddr = basic_blocks_.rbegin()->second.end_addr();
+  const size_t kRegionSize = kRegionEndAddr - kRegionStartAddr;
+
+  ArenaVector<bool> visited_target_offsets(kRegionSize, false, arena_);
+
+  worklist_targets.push_back(kRegionStartAddr);
+
+  // Collect reachable branch targets.
+  while (!worklist_targets.empty()) {
+    GuestAddr branch_addr = worklist_targets.front();
+    worklist_targets.pop_front();
+
+    auto it = basic_blocks_.find(branch_addr);
+    if (it == basic_blocks_.end()) {
+      continue;
+    }
+
+    CHECK_GE(branch_addr, kRegionStartAddr);
+    CHECK_LT(branch_addr, kRegionEndAddr);
+    if (visited_target_offsets.at(branch_addr - kRegionStartAddr)) {
+      continue;
+    }
+
+    const auto& basic_block = it->second;
+    worklist_targets.insert(
+        worklist_targets.end(), basic_block.out_edges().begin(), basic_block.out_edges().end());
+    reachable_targets.insert(basic_block.out_edges().begin(), basic_block.out_edges().end());
+    visited_target_offsets[branch_addr - kRegionStartAddr] = true;
+  };
+
+  return reachable_targets;
+}
+
+void GuestCodeRegion::RemoveUnreachableBlocks() {
+  CHECK(!basic_blocks_.empty());
+
+  auto branch_targets = CollectReachableBranchTargets();
+
+  // Remove unreachable basic_blocks.
+  auto bb_it = basic_blocks_.begin();
+  // Always keep the first basic block.
+  ++bb_it;
+
+  while (bb_it != basic_blocks_.end()) {
+    if (branch_targets.contains(bb_it->first)) {
+      ++bb_it;
+    } else {
+      bb_it = basic_blocks_.erase(bb_it);
+    }
+  }
+
+  // Update branch_targets.
+  branch_targets_ = std::move(branch_targets);
 }
 
 void GuestCodeRegion::SplitBasicBlocks() {
@@ -86,6 +153,42 @@ void GuestCodeRegion::ValidateRegionBeforeFinalize() const {
     last_seen_end_addr = basic_block.end_addr();
     CHECK(basic_block.in_edges().empty());
   }
+}
+
+std::string GuestCodeRegion::GetDebugString() const {
+  std::string out;
+  out += StringPrintf("BasicBlocks: { size=%zd, elements=[", basic_blocks_.size());
+  for (const auto& basic_block : basic_blocks_) {
+    out += " ";
+    out += basic_block.second.GetDebugString();
+  }
+
+  out += "]}\n";
+  out += "branch_targets={";
+  for (auto addr : branch_targets_) {
+    out += StringPrintf(" %zx", addr);
+  }
+
+  out += "}";
+
+  return out;
+}
+
+std::string GuestCodeBasicBlock::GetDebugString() const {
+  std::string out = "(";
+  out += StringPrintf("start=%zx, size=0x%zx(%zd), ", start_addr_, size_, size_);
+  out += "in_edges={";
+  for (auto addr : in_edges_) {
+    out += StringPrintf(" %zx", addr);
+  }
+  out += " }, out_edges={";
+
+  for (auto addr : out_edges_) {
+    out += StringPrintf(" %zx", addr);
+  }
+
+  out += " })";
+  return out;
 }
 
 }  // namespace berberis
