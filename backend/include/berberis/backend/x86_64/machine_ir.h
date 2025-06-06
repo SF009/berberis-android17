dc726d7bf32a3e8443efbc6402ac7d98c69c1b31
Richard Fung <richardfung@google.com>
2025-06-06 19:18:42 +0000

    Move IsCPUStateGet/Put to MachineIR
    This makes it so that we are checking the opcode before as cast to
    MachineInsnX86_64
    
    Flag: EXEMPT NDK
    Bug: 421217496
    Test: m berberis_all
    Change-Id: Ia45d28cff50b9dc86d8d9cccefefc8d7964ad504

--- backend/include/berberis/backend/x86_64/machine_ir.h
+++ backend/include/berberis/backend/x86_64/machine_ir.h
@@ -348,6 +348,60 @@ class MachineIR : public berberis::MachineIR {
     bb_order_ = BasicBlockOrder::kUnordered;
   }
 
+  [[nodiscard]] bool IsCPUStateGet(MachineInsn* insn) const {
+    if (insn->opcode() != kMachineOpMovqRegMemBaseDisp &&
+        insn->opcode() != kMachineOpMovdqaXRegMemBaseDisp &&
+        insn->opcode() != kMachineOpMovwRegMemBaseDisp &&
+        insn->opcode() != kMachineOpMovsdXRegMemBaseDisp) {
+      return false;
+    }
+
+    auto x86_insn = AsMachineInsnX86_64(insn);
+
+    // Check that it is not for ThreadState fields outside of CPUState.
+    if (x86_insn->disp() >= sizeof(CPUState)) {
+      return false;
+    }
+
+    // reservation_value is loaded in HeavyOptimizerFrontend::AtomicLoad and written
+    // in HeavyOptimizerFrontend::AtomicStore partially (for performance
+    // reasons), which is not supported by our context optimizer.
+    auto reservation_value_offset = offsetof(ThreadState, cpu.reservation_value);
+    if (x86_insn->disp() >= reservation_value_offset &&
+        x86_insn->disp() < reservation_value_offset + sizeof(Reservation)) {
+      return false;
+    }
+
+    return x86_insn->RegAt(1) == kCPUStatePointer;
+  }
+
+  [[nodiscard]] bool IsCPUStatePut(MachineInsn* insn) const {
+    if (insn->opcode() != kMachineOpMovqMemBaseDispReg &&
+        insn->opcode() != kMachineOpMovdqaMemBaseDispXReg &&
+        insn->opcode() != kMachineOpMovwMemBaseDispReg &&
+        insn->opcode() != kMachineOpMovsdMemBaseDispXReg) {
+      return false;
+    }
+
+    auto x86_insn = AsMachineInsnX86_64(insn);
+
+    // Check that it is not for ThreadState fields outside of CPUState.
+    if (x86_insn->disp() >= sizeof(CPUState)) {
+      return false;
+    }
+
+    // reservation_value is loaded in HeavyOptimizerFrontend::AtomicLoad and written
+    // in HeavyOptimizerFrontend::AtomicStore partially (for performance
+    // reasons), which is not supported by our context optimizer.
+    auto reservation_value_offset = offsetof(ThreadState, cpu.reservation_value);
+    if (x86_insn->disp() >= reservation_value_offset &&
+        x86_insn->disp() < reservation_value_offset + sizeof(Reservation)) {
+      return false;
+    }
+
+    return x86_insn->RegAt(0) == kCPUStatePointer;
+  }
+
   [[nodiscard]] MachineBasicBlock* NewBasicBlock() {
     return NewInArena<MachineBasicBlock>(arena(), arena(), ReserveBasicBlockId());
   }
