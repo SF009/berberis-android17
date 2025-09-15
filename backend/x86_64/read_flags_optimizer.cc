09e4a7da876a861da3e1333d475e85087cd65310
Richard Fung <richardfung@google.com>
2025-09-15 17:07:01 +0000

    READFLAGS opt: Check live_out for copied registers in CheckPostLoopNode
    Flag: NONE b/442633873
    Bug: 443353728
    Test: CODM doesn't crash anymore
    Change-Id: I40848c9f712bb099fb04ba14ae0cced1e318298c

--- backend/x86_64/read_flags_optimizer.cc
+++ backend/x86_64/read_flags_optimizer.cc
@@ -116,9 +116,19 @@ bool CheckPostLoopNode(MachineBasicBlock* bb, const MachineRegVector& regs) {
   if (bb->in_edges().size() != 1) {
     return false;
   }
+
+  // Keep track of copies to check live_out since we don't call
+  // CheckRegsUnusedWithinInsnRange.
+  MachineRegVector copies(regs.get_allocator());
+  for (auto* insn : bb->insn_list()) {
+    if ((insn->opcode() == kMachineOpPseudoCopy) && Contains(regs, insn->RegAt(1))) {
+      copies.push_back(insn->RegAt(0));
+    }
+  }
+
   // Check that it's not live_out.
   for (auto r : bb->live_out()) {
-    if (Contains(regs, r)) {
+    if (Contains(regs, r) || Contains(copies, r)) {
       return false;
     }
   }
