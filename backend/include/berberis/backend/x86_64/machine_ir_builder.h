6384b6d6b50c7b7487efd7d0c161a50763274fa7
Victor Khimenko <khim@google.com>
2026-01-10 20:45:42 +0000

    Add DependentType typedef to dependent_false.h
    And use it to clarify the code in dependent_false.h
    
    Test: m berberis_all
    
    Change-Id: If1b4340f851cfb2843b61919a78231229ccb5354

--- backend/include/berberis/backend/x86_64/machine_ir_builder.h
+++ backend/include/berberis/backend/x86_64/machine_ir_builder.h
@@ -77,8 +77,7 @@ class MachineIRBuilder : public MachineIRBuilderBase<MachineIR> {
               if (scratch_arg_idx >= 2) {
                 FATAL("Only two scratch registers are supported for now");
               }
-              using ThreadState =
-                  std::enable_if_t<!kDependentTypeFalse<Binding>, berberis::ThreadState>;
+              using ThreadState = DependentType<berberis::ThreadState, Binding>;
               return std::tuple{x86_64::MemoryOperand{
                   .base = x86_64::kMachineRegRBP,
                   .disp = static_cast<int32_t>(offsetof(ThreadState, intrinsics_scratch_area) +
@@ -106,7 +105,7 @@ class MachineIRBuilder : public MachineIRBuilderBase<MachineIR> {
             }
             if constexpr (HaveOutput(Binding::kArgInfo)) {
               static_assert(Operand::kUsage == device_arch_info::kUseDef);
-              std::enable_if_t<!kDependentTypeFalse<Binding>, MachineReg> out_reg;
+              DependentType<MachineReg, Binding> out_reg;
               if constexpr (device_arch_info::kIsFLAGS<Operand>) {
                 out_reg = flags_register;
               } else {
@@ -129,7 +128,7 @@ class MachineIRBuilder : public MachineIRBuilderBase<MachineIR> {
             static_assert(device_arch_info::kIsRegister<Operand>);
             static_assert(Operand::kUsage == device_arch_info::kDef ||
                           Operand::kUsage == device_arch_info::kDefEarlyClobber);
-            std::enable_if_t<!kDependentTypeFalse<Binding>, MachineReg> out_reg;
+            DependentType<MachineReg, Binding> out_reg;
             if constexpr (device_arch_info::kIsFLAGS<Operand>) {
               out_reg = flags_register;
             } else {
