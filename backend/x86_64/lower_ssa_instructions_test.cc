1a3d077f5ab5bd1822360e864a154044cf2948aa
Victor Khimenko <khim@google.com>
2026-02-17 16:34:25 +0000

    Add CheckSSA test
    Test: m berberis_all
    
    Change-Id: I9b5e28a8d06b4479f601fe40764d0214585b9489

--- backend/x86_64/lower_ssa_instructions_test.cc
+++ backend/x86_64/lower_ssa_instructions_test.cc
@@ -50,6 +50,118 @@ TEST(LowerSSAInstructionsTest, LowerAddqRegReg) {
   EXPECT_EQ(bb->insn_list().back()->opcode(), kMachineOpAddqRegReg);
 }
 
+TEST(LowerSSAInstructionsCheckTest, NonVReg) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+
+  x86_64::MachineIRBuilder builder(&machine_ir);
+
+  auto* bb = machine_ir.NewBasicBlock();
+  builder.StartBasicBlock(bb);
+
+  bb->live_in().push_back(MachineRegs::kRAX);
+  EXPECT_FALSE(CheckSSA(&machine_ir));
+}
+
+TEST(LowerSSAInstructionsCheckTest, HardRegInLiveIns) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+
+  x86_64::MachineIRBuilder builder(&machine_ir);
+
+  auto* bb = machine_ir.NewBasicBlock();
+  builder.StartBasicBlock(bb);
+
+  auto vreg = machine_ir.AllocVReg();
+
+  bb->live_in().push_back(vreg);
+  EXPECT_TRUE(CheckSSA(&machine_ir));
+  bb->live_in().push_back(vreg);
+  EXPECT_FALSE(CheckSSA(&machine_ir));
+}
+
+TEST(LowerSSAInstructionsCheckTest, UseDefReg) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+
+  x86_64::MachineIRBuilder builder(&machine_ir);
+
+  auto* bb = machine_ir.NewBasicBlock();
+  builder.StartBasicBlock(bb);
+
+  MachineReg vreg_dest = machine_ir.AllocVReg();
+  MachineReg vreg_src = machine_ir.AllocVReg();
+  MachineReg flags = machine_ir.AllocVReg();
+
+  builder.StartBasicBlock(bb);
+  builder.Gen<AddqRegReg, kNoSSA>(vreg_dest, vreg_src, flags);
+
+  EXPECT_FALSE(CheckSSA(&machine_ir));
+}
+
+TEST(LowerSSAInstructionsCheckTest, HardRegDefReg) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+
+  x86_64::MachineIRBuilder builder(&machine_ir);
+
+  auto* bb = machine_ir.NewBasicBlock();
+  builder.StartBasicBlock(bb);
+
+  MachineReg vreg_src1 = machine_ir.AllocVReg();
+  MachineReg vreg_src2 = machine_ir.AllocVReg();
+  MachineReg flags = machine_ir.AllocVReg();
+
+  builder.StartBasicBlock(bb);
+  // Note: we allow “use” for hardware register to access stack with bp, context with bp and so on.
+  // But all “def” registers should be vregs before lowering.
+  builder.Gen<AddqRegReg, kSSA>(MachineRegs::kRAX, vreg_src1, vreg_src2, flags);
+
+  EXPECT_FALSE(CheckSSA(&machine_ir));
+}
+
+TEST(LowerSSAInstructionsCheckTest, HardRegUseReg) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+
+  x86_64::MachineIRBuilder builder(&machine_ir);
+
+  auto* bb = machine_ir.NewBasicBlock();
+  builder.StartBasicBlock(bb);
+
+  MachineReg vreg_dest = machine_ir.AllocVReg();
+  MachineReg vreg_src = machine_ir.AllocVReg();
+  MachineReg flags = machine_ir.AllocVReg();
+
+  builder.StartBasicBlock(bb);
+  // Note: we allow “use” for hardware register to access stack with bp, context with bp and so on.
+  // But all “def” registers should be vregs before lowering.
+  builder.Gen<AddqRegReg, kSSA>(vreg_dest, MachineRegs::kRAX, vreg_src, flags);
+
+  EXPECT_TRUE(CheckSSA(&machine_ir));
+}
+
+TEST(LowerSSAInstructionsCheckTest, DoubleDefReg) {
+  Arena arena;
+  x86_64::MachineIR machine_ir(&arena);
+
+  x86_64::MachineIRBuilder builder(&machine_ir);
+
+  auto* bb = machine_ir.NewBasicBlock();
+  builder.StartBasicBlock(bb);
+
+  MachineReg vreg_dest = machine_ir.AllocVReg();
+  MachineReg vreg_src1 = machine_ir.AllocVReg();
+  MachineReg vreg_src2 = machine_ir.AllocVReg();
+  MachineReg flags = machine_ir.AllocVReg();
+
+  builder.StartBasicBlock(bb);
+  builder.Gen<AddqRegReg, kSSA>(vreg_dest, vreg_src1, vreg_src2, flags);
+  builder.Gen<AddqRegReg, kSSA>(vreg_dest, vreg_src1, vreg_src2, flags);
+
+  EXPECT_FALSE(CheckSSA(&machine_ir));
+}
+
 }  // namespace
 
 }  // namespace berberis::x86_64
